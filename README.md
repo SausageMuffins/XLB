09165b34b3f3e7c91a1a3c4c25185e2ebc345148
SausageMuffins <rwang9098@gmail.com>
2024-02-23 17:25:29 +0200

    Added the generation component

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ This project seeks to investigate the possibility of biases in generated answers
 Language Models Tested:
 - ChatGPT 3.5 Turbo
 
+Embedding Models Tested:
+- text-embedding-3-small
 ## Set-up
 
 Follow the steps below to set up the necessary dependencies. LangChain quickstart guide [here](https://python.langchain.com/docs/use_cases/question_answering/quickstart).
@@ -19,3 +21,11 @@ Make sure to create your own .env file to store the necessary information for th
 API_KEY=insert_api_token_here
 ```
 Your OpenAPI key can be created [here](https://platform.openai.com/api-keys). Make sure to top up the balance to use the api key. 
+
+---
+
+## Relevant Files
+
+format_output.py is mainly for formatting and visualizing outputs at different stages of the pipeline (loading, splitting, retrieving etc.). The visualized files are under the folder "visualizing_articles".
+
+Note: The reference articles are news articles with various topics (e.g. Covid-19, Climate Change). The articles will be used to in the main file (xlb) to perform RAG on.
--- xlb.py
+++ xlb.py
@@ -1,14 +1,16 @@
 import os
 import dotenv
 
-import bs4
-from langchain import hub
+# loader, splitter, vectorstore
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.vectorstores import Chroma
+
+# prompts
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings # models
 from getpass import getpass
 
 from format_output import format_and_write_chunks, format_and_write_docs, format_and_write_results # for formatting and readability
@@ -36,8 +38,11 @@ def split_text(docs):
     #print(chunks) - I found out that chunks are a list of Document objects
     return chunks
 
-model = ChatOpenAI()
-embeddings = OpenAIEmbeddings()
+model = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0,
+    )
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
 query = "What should I know about Covid-19 now?"
 sep = "-" * 50
@@ -45,7 +50,7 @@ sep = "-" * 50
 # Loop through directories and query each topic
 for root, directory, files in os.walk("reference_articles"):
     for folder in directory: # Repeat for each topic - JN1 or Vaccines
-        print(f"{sep} + \n + Processing {folder}...")
+        print(f"{sep} \n + Processing {folder}...")
         loaded_docs = load_documents(folder)
         chunks = split_text(loaded_docs)
         
@@ -54,13 +59,33 @@ for root, directory, files in os.walk("reference_articles"):
         format_and_write_chunks(chunks, file_path=f"visualizing_articles/{folder}/{folder}_chunks.txt")
         
         db = Chroma.from_documents(chunks, embeddings) # create a new vector store each time to avoid keeping the old context
-        retriever = db.as_retriever(search_type="mmr", search_kwargs={"k": 8}) # mmr, top 8 results
+        retriever = db.as_retriever(
+            search_type="mmr", # algo to calculate similarity
+            search_kwargs={"k": 4} # top k results
+            )
         results = retriever.get_relevant_documents(query) 
         retrieved_results = ""
-        
         
         for ranking, doc in enumerate(results):
             retrieved_results += f"Ranking {ranking + 1}: {doc.metadata} \n{doc.page_content}\n"
             retrieved_results += "\n" + sep + "\n\n"
         
         format_and_write_results(retrieved_results, file_path=f"visualizing_articles/{folder}/{folder}_results.txt")
+        
+        
+        # Generation Component
+        context = " ".join([doc.page_content for doc in results])
+        combined_prompt = "Given the context: {context}, \n answer the following question: {query}"
+        
+        prompt = ChatPromptTemplate.from_template(combined_prompt)
+        
+        answer_RAG = prompt | model | StrOutputParser() # using LCEL for future prompt chaining
+        # TODO: chain together with the evaluation component - need the prompt + the defined LCEL chain from LC
+        response = answer_RAG.invoke({"context":context, "query":query})
+        
+        print(f"Response: {response}")
+        
+        # Write the response to a file
+        with open(f"visualizing_articles/{folder}/response.txt", "w") as file:
+            file.write(response)
+        
