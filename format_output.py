0dc8e2edddc1b4e8b3e362561a5c25192f0420ca
SausageMuffins <rwang9098@gmail.com>
2024-02-23 11:27:47 +0200

    updated format_output.py to output loaded docs

--- format_output.py
+++ format_output.py
@@ -1,7 +1,7 @@
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-def format_and_write_docs(docs, file_path='visualizing_articles/nytimes_article.txt'):
+def format_and_write_docs(docs, file_path="visualizing_articles/loaded_docs.txt"):
 
     formatted_content = "\n\n".join(doc.page_content for doc in docs)
     with open(file_path, 'w', encoding='utf-8') as file:
